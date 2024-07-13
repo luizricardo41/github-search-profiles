a252015b3f5fb57fa60651dcf294d8921574feff
Luiz Ricardo <luizricardo41@gmail.com>
2024-07-13 18:28:05 -0300

    Criaçaõ de pasta e componente Nav

---
+++ src/app/components/Nav/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className="py-10 border-2 w-full">
+        <nav className="py-4 px-16 flex justify-between border-2">
+          <h2>Icon</h2>
+          <h2>Theme Light/Dark</h2>
+        </nav>
+      </div>
+  )
+}
