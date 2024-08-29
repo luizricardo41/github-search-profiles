cc5bbb0a0eb7acb3e929336089d43aa5fd989866
Luiz Ricardo <luizricardo41@gmail.com>
2024-08-28 21:17:45 -0300

    Estilização das paginas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Nav/Navbar";
 export default function Home() {
 
   return (
-    <main className="flex items-center flex-col w-full h-screen">
+    <main className="flex items-center flex-col w-full h-screen bg-[#0d1117]">
       <Navbar />
       <h1 className="text-2xl font-bold">Encontre um perfil do GitHub pelo nome de usuário!</h1>
       <Form />
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,33 +1,62 @@
 "use client"
 import React from 'react'
 import { ProfileProvider } from '../hooks/useProfile'
+import GitHub from '@/icons/GitHub'
+import { BookMarked, Ellipsis, Inbox, Menu, Search } from 'lucide-react'
+import image from '../../../public/images/images-portifolio3.png'
+import Image from 'next/image'
 
 export default function profile() {
   return (
     <ProfileProvider>
       <header className='bg-[#0d1117]'>
-        <div className='mr-5 p-3 bg-black border-b-2 border-slate-500'>
+        <div className='mr-5 p-3 bg-black border-b-[1px] border-slate-600'>
           <div className='flex items-center justify-start w-full pb-3'>
             <div className='flex items-center gap-4 w-1/2'>
-              <div className='min-w-7 min-h-7 border-2 rounded-md'></div>
-              <div className='min-w-7 min-h-7 border-2 rounded-full'></div>
+              <button className='min-w-8 min-h-8 border-[1px] border-slate-600 rounded-md flex justify-center items-center'>
+                <Menu className='text-slate-600' size={20} />
+              </button>
+              <div className='min-w-7'>
+                <GitHub />
+              </div>
               <h3 className='max-w-fit overflow-hidden text-ellipsis font-bold text-sm text-white'>luizricardo41</h3>
             </div>
             <div className='flex items-center justify-end gap-3 w-1/2 pl-2'>
-              <div className='min-w-7 min-h-7 border-2 rounded-md'></div>
-              <div className='min-w-7 min-h-7 border-2 rounded-md'></div>
-              <div className='min-w-7 min-h-7 border-2 rounded-full'></div>
+              <button className='min-w-8 min-h-8 border-[1px] border-slate-600 rounded-md flex justify-center items-center'>
+                <Search className='text-slate-600' size={18} />
+              </button>
+              <button className='min-w-8 min-h-8 border-[1px] border-slate-600 rounded-md flex justify-center items-center'>
+                <Inbox className='text-slate-600' size={18} />
+              </button>
+              <div className='w-8 h-8 rounded-full'>
+                <Image
+                  alt='image'
+                  src={image}
+                  className='rounded-full w-8 h-8'
+                />
+              </div>
             </div>
           </div>
           <div className='flex items-center justify-between w-full'>
-            <h3 className='text-white text-sm'>Overview</h3>
-            <div className='w-7 h-7 border-1 rounded-md text-white text-center font-extrabold bg-slate-800 opacity-75'>...</div>
+            <div className='flex gap-2'>
+              <BookMarked className='text-slate-600' size={18} />
+              <h3 className='text-white text-sm'>Repositories</h3>
+            </div>
+            <button className='min-w-8 min-h-8 border-[1px] border-slate-600 rounded-md flex justify-center items-center'>
+              <Ellipsis className='text-slate-600' size={18} />
+            </button>
           </div>
         </div>
       </header>
       <section className='py-14 px-5'>
         <div className='flex items-center'>
-          <div className='w-12 h-12 border-2 rounded-full'></div>
+          <div className='min-w-3 min-h-3 rounded-full w-2/12'>
+            <Image
+              alt='image'
+              src={image}
+              className='rounded-full min-w-12 min-h-12'
+            />
+          </div>
           <div className='px-3'>
             <h1 className='text-white text-xl font-bold'>Luiz Ricardo</h1>
             <h3 className='text-slate-400 text-lg'>luizricardo41</h3>
